05ca3824bc6bb16b9bdaab090ea6ea2dc1be4153
pedronmb <pedro.nmb@gmail.com>
2024-09-22 22:43:40 -0300

    estilos del login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,13 +8,20 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymus">
 
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     </head>
     <body>
+        <div class="container vh-100">
+        <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-12 col-md-6 col-lg-4">
+        
         <main class="container align-center p-5">
+        <img src="{{ asset('img/logo.png') }}" alt="Logo" class="img-fluid mb-4 d-block mx-auto" style="max-width: 150px;">
             <form method="POST" action="{{route('inicia-sesion')}}">
                 @csrf
+                
                 <div class="mb-3">
-                    <label for="emailInput" class="form-label">Emaile</label>
+                    <label for="emailInput" class="form-label">Email</label>
                     <input type="email" class="form-control" id="emailInput" name="email" required>
                 </div>
                 <div class="mb-3">
@@ -31,6 +38,9 @@
                 <button type="submit" class="btn btn-primary">Acceder</buttton>
             </form>
         </main>
+        </div>
+        </div>
+        </div>
     </body>
     
 </html>
